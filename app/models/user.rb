fa59677891be4c9c06b81bdc03e6e6ece663317b
dodoya1 <98104380+dodoya1@users.noreply.github.com>
2022-12-07 17:59:12 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
-  # has_secure_passwordメソッドを追加してください
   has_secure_password
   
   validates :name, {presence: true}
   validates :email, {presence: true, uniqueness: true}
-  # 以下のバリデーションを削除してください
   
   
   def posts
